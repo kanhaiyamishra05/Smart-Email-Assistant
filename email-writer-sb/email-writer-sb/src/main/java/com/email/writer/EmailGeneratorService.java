97307d02e0e41e9fedcb44a297a13adb3f8c1031
mishr <kanhaiyakumar993197@gmail.com>
2026-05-03 04:08:27 +0530

    Initial commit: smart-email-assistant

---
+++ email-writer-sb/email-writer-sb/src/main/java/com/email/writer/EmailGeneratorService.java
@@ -0,0 +1,84 @@
+package com.email.writer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class EmailGeneratorService {
+
+    private final String apiKey;
+    private final WebClient webClient;
+
+    public EmailGeneratorService(WebClient.Builder webClientBuilder,
+                                 @Value("${gemini.api.url}") String baseUrl,
+                                 @Value("${gemini.api.key}") String geminiApiKey) {
+        this.apiKey = geminiApiKey;
+        this.webClient = webClientBuilder.baseUrl(baseUrl).build();
+    }
+
+    public String generateEmailReply(EmailRequest emailRequest) {
+//        Build prompt
+        String prompt= buildPrompt(emailRequest);
+
+//        prepare a raw json body
+
+        String requestBody = String.format("""
+                {
+                    "contents": [
+                      {
+                        "parts": [
+                          {
+                            "text": "%s"
+                          }
+                        ]
+                      }
+                    ]
+                  }""", prompt);
+//        send request
+
+        String response= webClient.post()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/v1beta/models/gemini-3-flash-preview:generateContent")
+                        .build())
+                .header("x-goog-api-key",apiKey)
+                .header("Content-Type","application/json")
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+//        extract response
+        return extractResponseContent(response);
+    }
+
+    private String extractResponseContent(String response) {
+        try {
+            ObjectMapper mapper=new ObjectMapper();
+            JsonNode root=mapper.readTree(response);
+           return root.path("candidates")
+                    .get(0)
+                    .path("content")
+                    .path("parts")
+                    .get(0)
+                    .path("text")
+                    .asText();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String buildPrompt(EmailRequest emailRequest) {
+        StringBuilder prompt= new StringBuilder();
+        prompt.append("Generate a professional email reply for the following email without subject and only one reply:");
+        if(emailRequest.getTone() != null && emailRequest.getTone().isEmpty()){
+            prompt.append("Use a ").append(emailRequest.getTone()).append("Tone.");
+//            use a professional tone.
+        }
+        prompt.append("Original Email: \n ").append(emailRequest.getEmailContent());
+        return prompt.toString();
+    }
+}
